cc63b1ed0741a0733d74b2987de1fdede2bb5313
pepesan <pepesan@gmail.com>
2024-11-26 18:05:42 +0100

    Ejemplos de componentes basados en clases y en funciones y en constantes de funciones anónimas

---
+++ src/components/ConstComponent.tsx
@@ -0,0 +1,7 @@
+// const SimpleComponent = ({ message }) => {
+const ConstComponent = () => {
+    // que presenta un div con el contenido en texto de message
+    return <div>ConstComponente</div>;
+};
+
+export default ConstComponent;
---
+++ src/components/Sith.tsx
@@ -0,0 +1,12 @@
+import "./Sith.css";
+// componente de tipo Function Component
+function Sith (){
+    // devuelve el valor de la plantilla
+    return(
+        <div>
+            <p className="sith-box"> I'm Bad, you know!</p>
+        </div>
+    )
+}
+// exportar el componenter para pueda ser cargado
+export default Sith;
