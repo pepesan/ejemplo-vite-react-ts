7023ad79b393fb75f7ea9b99f9d4fa616c1922ba
pepesan <pepesan@gmail.com>
2024-11-28 17:26:48 +0100

    usado un componente para integrar react-boostrap con el sistema de navegacion de react-router(-dom)

--- src/components/pages/Menu.tsx
+++ src/components/pages/Menu.tsx
@@ -1,14 +1,45 @@
-import {Link} from "react-router";
-
+// Ref : https://github.com/react-bootstrap/react-router-bootstrap
+// Uso esto de base porque quiero que los enlaces de Link vayan dentro de un NAvBar de React Bootstrap
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import "./Menu.css"
+import LinkContainer from "./LinkContainer.tsx";
 const Menu = () => {
     return (
-        <nav>
-            <Link to="/">Inicio</Link>|{" "}
-            <Link to="/about">About</Link> |{" "}
-            <Link to="/navega">Navega</Link> |{" "}
-            <Link to="/listado">Listado</Link> |{" "}
-            <Link to="/ciclodevida">Ciclo de Vida</Link> |{" "}
-        </nav>
+        <div>
+
+            <Navbar expand="lg" className="bg-body-tertiary">
+                <Container>
+                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <LinkContainer to="/">
+                                <Nav.Link>Inicio</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to="/about">
+                                <Nav.Link>About</Nav.Link>
+                            </LinkContainer>
+                            <NavDropdown title="Navegacion" id="basic-nav-dropdown">
+                                <LinkContainer to="/navega">
+                                    <NavDropdown.Item>Navega</NavDropdown.Item>
+                                </LinkContainer>
+                                <LinkContainer to="/listado">
+                                    <NavDropdown.Item>
+                                        Listado
+                                    </NavDropdown.Item>
+                                </LinkContainer>
+                            </NavDropdown>
+                            <LinkContainer to="/ciclodevida">
+                                <Nav.Link >Ciclo de Vida</Nav.Link>
+                            </LinkContainer>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
     )
 }
 
